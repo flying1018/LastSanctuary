7ed472c07854f3f5f463ea404225116aeab9d224
sunyeji <ssuu0167@gmail.com>
2025-06-26 11:43:46 +0900

    [add] Dash 폴더 추가
    Dash폴더 생성해서 PlayerDashState 스크립트 추가

---
+++ LastSanctuary/Assets/01.Scipts/Player/PlayerState/Dash/PlayerDashState.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDashState : PlayerBaseState
+{
+    public PlayerDashState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+    
+    public override void Enter()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+    public override void HandleInput()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void PhysicsUpdate()
+    {
+
+    }
+}
