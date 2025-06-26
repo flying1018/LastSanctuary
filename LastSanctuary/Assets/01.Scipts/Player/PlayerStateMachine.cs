abf58249c9f4e910a304949c861c6555f76cf9fb
halonine9 <dlselhvxg@gmail.com>
2025-06-26 15:08:24 +0900

    [update] 가드 상태 작성중

--- LastSanctuary/Assets/01.Scipts/Player/PlayerStateMachine.cs
+++ LastSanctuary/Assets/01.Scipts/Player/PlayerStateMachine.cs
@@ -7,6 +7,7 @@ public class PlayerStateMachine : StateMachine
     public PlayerMoveState MoveState { get; private set; }
     public PlayerJumpState JumpState { get; private set; }
     public PlayerAttackState AttackState { get; private set; }
+    public PlayerGuardState GuardState { get; private set; }
 
     public PlayerStateMachine(Player player)
     {
