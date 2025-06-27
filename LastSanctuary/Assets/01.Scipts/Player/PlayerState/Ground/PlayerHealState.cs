90c938e30dc87475c349c34487c6d31351dcec87
sunyeji <ssuu0167@gmail.com>
2025-06-27 14:39:06 +0900

    [update] 스크립트 내용추가
    PlayerHealState 안에 구문 추가

--- LastSanctuary/Assets/01.Scipts/Player/PlayerState/Ground/PlayerHealState.cs
+++ LastSanctuary/Assets/01.Scipts/Player/PlayerState/Ground/PlayerHealState.cs
@@ -7,4 +7,29 @@ public class PlayerHealState : PlayerGroundState
     public PlayerHealState(PlayerStateMachine stateMachine) : base(stateMachine)
     {
     }
+    
+    public override void Enter()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+    public override void HandleInput()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void PhysicsUpdate()
+    {
+
+    }
 }
