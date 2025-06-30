609e282b78e6145c8d1372bce55139c53d5b9780
leejy1685 <ucc1685@naver.com>
2025-06-30 19:40:35 +0900

    [fix] : 공격 속도 조절

--- LastSanctuary/Assets/01.Scipts/Player/PlayerState/Air/PlayerLadderState.cs
+++ LastSanctuary/Assets/01.Scipts/Player/PlayerState/Air/PlayerLadderState.cs
@@ -49,6 +49,7 @@ public class PlayerLadderState : PlayerAirState
     
     public override void PhysicsUpdate()
     {
+        base.PhysicsUpdate();
         VerticalMove(_input.MoveInput);
     }
 
--- LastSanctuary/Assets/01.Scipts/Player/PlayerWeapon.cs
+++ LastSanctuary/Assets/01.Scipts/Player/PlayerWeapon.cs
@@ -12,7 +12,7 @@ public class PlayerWeapon : MonoBehaviour
         if (other.TryGetComponent(out IDamageable idamageable) )
         {
             if (other.gameObject.CompareTag(StringNameSpace.Tags.Player)) return;
-            Debug.Log(idamageable);
+            Debug.Log(idamageable + "는 " + Damage +"의 를 받았다.");
             idamageable.TakeDamage(Damage, DamageType.Contact);
         }
     }
