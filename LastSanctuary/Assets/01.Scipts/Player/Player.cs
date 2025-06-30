c6044ba2dd5cf9ca5b395a180f276a29987773e6
jk1018 <1018jkjk@gmail.com>
2025-06-30 17:06:32 +0900

    [feat] 플레이어 부활 로직

--- LastSanctuary/Assets/01.Scipts/Player/Player.cs
+++ LastSanctuary/Assets/01.Scipts/Player/Player.cs
@@ -6,12 +6,12 @@ public class Player : MonoBehaviour
 {
     //필드
     private PlayerStateMachine _stateMachine;
-    
+
     //직렬화
     [field: SerializeField] public PlayerAnimationDB AnimationDB { get; private set; }
     [SerializeField] private PlayerSO playerData;
     [SerializeField] private GameObject playerModel;
-    
+
     //프로퍼티
     public PlayerController Input { get; set; }
     public PlayerSO Data { get => playerData; }
@@ -19,8 +19,8 @@ public class Player : MonoBehaviour
     public Animator Animator { get; set; }
     public SpriteRenderer SpriteRenderer { get; set; }
     public PlayerCondition Condition { get; set; }
-    public GameObject Model { get=> playerModel; }
-    
+    public GameObject Model { get => playerModel; }
+
 
     private void Awake()
     {
@@ -52,22 +52,38 @@ public class Player : MonoBehaviour
     /// </summary>
     private void OnDie()
     {
+        DebugHelper.Log("OnDie 호출됨");
+
+        Condition.ChangeInvincible(true);
         Animator.SetTrigger("@Die");
         Input.enabled = false;
+
         Rigidbody.velocity = Vector2.zero;
-        // 추후 죽었을때 표기되는 UI추가 요망
+        // 추후 죽었을때 표기되는 UI, 연출 추가 요망 (화면 암전 등)
+        StartCoroutine(Revive_Coroutine());
     }
 
     /// <summary>
     /// 리스폰할때 실행되는 코루틴
     /// </summary>
     /// <returns></returns>
-    private IEnumerator Respawn_Coroutine()
+    private IEnumerator Revive_Coroutine()
     {
-        yield return new WaitForSeconds(2f);
-        transform.position = SaveManager.Instance.GetSavePoint();
+        DebugHelper.Log("Revive_Coroutine 호출됨");
 
-        Animator.SetTrigger("@Respawn");
+        yield return new WaitForSeconds(2f); // 앞 애니메이션 대기
+
+        transform.position = SaveManager.Instance.GetSavePoint();
+        Animator.SetTrigger("Respawn");
+        yield return new WaitForSeconds(2f);
+
+        Condition.PlayerRecovery();
+        _stateMachine.ChangeState(_stateMachine.IdleState);
+        Animator.Rebind();
+
         Input.enabled = true;
+        yield return new WaitForSeconds(0.5f);
+    
+        Condition.ChangeInvincible(false);
     }
 }
--- LastSanctuary/Assets/01.Scipts/Player/PlayerCondition.cs
+++ LastSanctuary/Assets/01.Scipts/Player/PlayerCondition.cs
@@ -8,30 +8,33 @@ public class PlayerCondition : MonoBehaviour, IDamageable
     private Player _player;
 
     public event Action OnDie;
-    
-    
+
+
     private int _curHp;
     private int _maxHp;
     private int _curStamina;
     private int _maxStamina;
     private int _totaldamage;
-    
+
     //test용
     private float _damageReduction = 0.8f;
     private int _steminaRecovery = 30;
     private int _staminaCost = 30;
 
-    public bool IsPerfectGuard {get; set;}
-    public bool IsGuard {get; set;}
-  
+    public bool IsPerfectGuard { get; set; }
+    public bool IsGuard { get; set; }
+    public bool IsInvincible { get; set; }
+
 
     private void Awake()
     {
         _player = GetComponent<Player>();
     }
-    
+
     public void TakeDamage(int atk, DamageType type)
     {
+        if (IsInvincible) { return; }
+
         if (IsPerfectGuard && type != DamageType.Contact)
         {
             Debug.Log("PerfectGuard");
@@ -49,10 +52,24 @@ public class PlayerCondition : MonoBehaviour, IDamageable
             _totaldamage = atk;
             _curHp -= _totaldamage;
             Debug.Log($"데미지를 받음{_totaldamage}");
-            if (_curHp <= _totaldamage)
+            if (_curHp <= 0)
             {
                 OnDie?.Invoke();
             }
         }
+    }
+
+    public void PlayerRecovery()
+    {
+        DebugHelper.Log($"1. {_curHp} 현재 체력");
+        _curHp = _maxHp;
+        _curStamina = _maxStamina;
+
+        DebugHelper.Log($"2. {_curHp} 현재 체력");
+    }
+
+    public void ChangeInvincible(bool Invincible)
+    {
+        IsInvincible = Invincible;
     }
 }
