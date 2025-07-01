fb76fdc0a3d420bdb5c07b13159870ed3ecaf142
jk1018 <1018jkjk@gmail.com>
2025-07-01 16:57:07 +0900

    [add] 인벤토리 요구 스크립트 생성

---
+++ LastSanctuary/Assets/01.Scipts/Item/ItemInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    None,
+    
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemInfo : ScriptableObject
+{
+
+}
