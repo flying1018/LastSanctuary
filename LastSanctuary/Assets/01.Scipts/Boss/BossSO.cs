bb1a3d86a745770fc366322653b319ced79345f8
leejy1685 <ucc1685@naver.com>
2025-07-07 19:10:58 +0900

    [add] : 보스 기본 상태 머신 추가

---
+++ LastSanctuary/Assets/01.Scipts/Boss/BossSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Boss", menuName = "new Boss")]
+public class BossSO : ScriptableObject
+{
+    [Header("Condition")] 
+    public int attack;
+    public int defense;
+    public int hp;
+    public int groggyGauge;
+
+}
