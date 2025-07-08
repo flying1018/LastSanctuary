6a729f18103531f8709b2c50a5169cd4c6b82ab4
halonine9 <dlselhvxg@gmail.com>
2025-07-08 10:48:30 +0900

    [refactor]  매니저 DontDestroyOnLoad 구분

--- LastSanctuary/Assets/01.Scipts/Manager/GameManager.cs
+++ LastSanctuary/Assets/01.Scipts/Manager/GameManager.cs
@@ -5,5 +5,12 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager>
 {
     public bool[] IsPlayerClear; // 테스트용 추후 삭제
+
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+
     public void Init() { }
 }
--- LastSanctuary/Assets/01.Scipts/Manager/MapManager.cs
+++ LastSanctuary/Assets/01.Scipts/Manager/MapManager.cs
@@ -9,39 +9,7 @@ public class MapManager : Singleton<MapManager>
 
     public WarpObject selectWarpObj;
     public WarpObject targetWarpObj;
-
-    private static MapManager _instance;
-    public static MapManager Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = FindObjectOfType<MapManager>();
-                if (_instance == null)
-                {
-                    DebugHelper.LogError("MapManager 없음");
-                }
-            }
-            return _instance;
-        }
-    }
-
-    private void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this;
-        }
-        else
-        {
-            if (_instance != this)
-            {
-                Destroy(gameObject);
-            }
-        }
-    }
-
+    
     public void RespawnEnemies()
     {
         foreach (var spawnPoint in SpawnPoints) spawnPoint.Respawn();
@@ -50,7 +18,6 @@ public class MapManager : Singleton<MapManager>
     public void WarpInteract()
     {
         targetWarpObj = warpObjects[UIManager.Instance.ShowWarpUI(selectWarpObj) - 1];
-
         //player.transform.position = targetWarpObj.WarpTransform.position;
 
     }
