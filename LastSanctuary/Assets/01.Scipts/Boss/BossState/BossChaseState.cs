f62b1c14774e93086a32ff97bc3e8114d8a78f82
sunyeji <ssuu0167@gmail.com>
2025-07-10 10:06:40 +0900

    [fix]움직임 코드수정
    움직임 값과 방향, 중간저장

--- LastSanctuary/Assets/01.Scipts/Boss/BossState/BossChaseState.cs
+++ LastSanctuary/Assets/01.Scipts/Boss/BossState/BossChaseState.cs
@@ -26,22 +26,25 @@ public class BossChaseState : BossBaseState
             _stateMachine.ChangeState(_stateMachine.IdleState);
             return;
         }
-        
-        Vector2 bossPos = _boss.transform.position;
-        Vector2 targetPos = _boss.Target.position;
-        targetPos.y = bossPos.y;
 
-        float distance = Vector2.Distance(bossPos, targetPos);
-        
-        if (distance <= _data.attackRange)// 일정 거리 안에 들어오면 Idle 상태로 전환
-        {
-            Debug.Log("가까워짐 대기상태 진입");
-            _rigidbody.velocity = Vector2.zero;
-            _stateMachine.ChangeState(_stateMachine.IdleState);
-            return;
-        }
-        
-        Vector2 direction = (targetPos - bossPos).normalized;
+        Vector2 direction = DirectionToTarget();
+        Rotate(direction);
+        Move(direction);
+    }
+    
+    public Vector2 DirectionToTarget()
+    {
+        if(_boss.Target == null) return Vector2.zero;
+       return _boss.Target.position - _boss.transform.position; //플레이어 방향
+    }
+    
+    public void Rotate(Vector2 direction)
+    {
+        _rigidbody.velocity = direction * _data.moveSpeed;
+    }
+
+    public void Move(Vector2 direction)
+    {
         _rigidbody.velocity = direction * _data.moveSpeed;
     }
 }
