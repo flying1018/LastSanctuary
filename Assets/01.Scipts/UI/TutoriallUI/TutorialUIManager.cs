a49fe7c32c72457c43220d2fa38441a4d82df934
halonine9 <dlselhvxg@gmail.com>
2025-07-30 20:00:01 +0900

    [feat] 튜토UI(이미지 출력)

--- Assets/01.Scipts/UI/TutoriallUI/TutorialUIManager.cs
+++ Assets/01.Scipts/UI/TutoriallUI/TutorialUIManager.cs
@@ -1,23 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class TutorialUIManager : MonoBehaviour
-{
-    [SerializeField] private GameObject uiBackground;
+{ 
+    [SerializeField] private GameObject UIGuideIcon;
+    [SerializeField] private Image uiIcon;
+    [SerializeField] private GameObject UIGuideImage;
     [SerializeField] private Image uiImage;
-    
+    //UI 스프라이트 및 위치
     [SerializeField] private List<Sprite> uiSprites;
-
-    public void ShowUI(int index)
+    [SerializeField] private List<Transform> uiTransforms;
+    [SerializeField] private Transform uiParent;
+    private void Awake()
     {
-      uiImage.sprite = uiSprites[index];
-      uiBackground.SetActive(true);
+        uiTransforms.Clear();
+        foreach (Transform trigger in uiParent)
+        {
+            if(trigger.childCount > 0)
+            {
+                uiTransforms.Add(trigger.GetChild(0));
+            }
+        }
+    }
+
+    public void ShowUI(int index, TUItype type)
+    {
+        HideUI();
+        
+      Sprite sprite = uiSprites[index];
+
+      if (type == TUItype.Repeat)
+      {
+          uiIcon.sprite = sprite;
+          if (index < uiTransforms.Count && uiTransforms[index] != null)
+          {
+              UIGuideIcon.transform.position = uiTransforms[index].position;
+          }
+          UIGuideIcon.SetActive(true);
+      }
+      else if (type == TUItype.Once)
+      {
+          uiImage.sprite = sprite;
+          UIGuideImage.SetActive(true);
+      }
     }
 
     public void HideUI()
     {
-        uiBackground.SetActive(false);
+        UIGuideIcon.SetActive(false);
+        UIGuideImage.SetActive(false);
     }
 }
