8c431af8f873bc4103622dd3b8df38532b180b8b
leejy1685 <ucc1685@naver.com>
2025-08-07 21:19:26 +0900

    [fix] : 빌드를 위해서 기능 하나 중지

--- Assets/01.Scipts/UI/TutoriallUI/TutorialUIPopup.cs
+++ Assets/01.Scipts/UI/TutoriallUI/TutorialUIPopup.cs
@@ -1,4 +1,4 @@
-
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,8 +6,14 @@ using UnityEngine.UI;
 public class TutorialUIPopup : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI[] texts;
+    [SerializeField] private Button closeButton;
     
     private Image _image;
+
+    private void Awake()
+    {
+        closeButton.onClick.AddListener(OnCloseButton);
+    }
 
     public void Init(Sprite sprite, string title, string explanation)
     {
@@ -19,5 +25,10 @@ public class TutorialUIPopup : MonoBehaviour
         _image.sprite = sprite;
         texts[0].text = title.ToString();
         texts[1].text = explanation.ToString();
+    }
+
+    private void OnCloseButton()
+    {
+        gameObject.SetActive(false);
     }
 }
