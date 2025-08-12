c440c12315abb5ad130689f7a7e234523e3a58ae
jk1018 <1018jkjk@gmail.com>
2025-08-12 12:41:05 +0900

    [fix] 힌트UI 오류수정

--- Assets/01.Scipts/Map/MapObject/HintObject.cs
+++ Assets/01.Scipts/Map/MapObject/HintObject.cs
@@ -1,13 +1,5 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Hint", menuName = "New Hint")]
-public class HintSO : ScriptableObject
-{
-    [Header("Info")]
-    public int index;
-    [TextArea(2, 10)] public string bodyText;
-}
-
 public class HintObject : MonoBehaviour
 {
     [Header("퍼즐 힌트")]
