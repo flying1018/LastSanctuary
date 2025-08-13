a2f2100b459aea13b64688c153ca241d69faa811
halonine9 <dlselhvxg@gmail.com>
2025-08-13 11:55:43 +0900

    [fix] 퍼즐 레버 토글 방식으로 수정

--- Assets/01.Scipts/Map/MapObject/MoveObject.cs
+++ Assets/01.Scipts/Map/MapObject/MoveObject.cs
@@ -11,7 +11,12 @@ public class MoveObject : MonoBehaviour
     {
 
     }
-    
+
+    public virtual void ReturnObj()
+    {
+        
+    }
+
     private void OnDisable()
     {
         StopAllCoroutines();
